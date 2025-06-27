ef080bb64cdf8c3afa3ee57f73e682076a416fca
Jay Park <pjh6183@naver.com>
2025-06-27 19:13:35 +0900

    Initial commit

---
+++ src/main/java/Hackathon/DopamineMarket/DopamineMarketApplication.java
@@ -0,0 +1,13 @@
+package Hackathon.DopamineMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamineMarketApplication.class, args);
+	}
+
+}
