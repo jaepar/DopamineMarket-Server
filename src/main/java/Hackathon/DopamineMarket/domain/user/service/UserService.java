d6b4d6248cf48013a15d28fd467669383bf4d964
kkb <gimgyubin1225@gmail.com>
2025-06-28 21:16:20 +0900

    [FEAT] login 로직 구현
    - LoginRequest를 사용하여 회원가입 or 로그인

--- src/main/java/Hackathon/DopamineMarket/domain/user/service/UserService.java
+++ src/main/java/Hackathon/DopamineMarket/domain/user/service/UserService.java
@@ -1,4 +1,42 @@
 package Hackathon.DopamineMarket.domain.user.service;
 
+import Hackathon.DopamineMarket.domain.user.domain.User;
+import Hackathon.DopamineMarket.domain.user.dto.request.LoginRequest;
+import Hackathon.DopamineMarket.domain.user.exception.InvalidPasswordException;
+import Hackathon.DopamineMarket.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User login(LoginRequest loginRequest) {
+        Optional<User> byNickname = userRepository.findByNickname(loginRequest.getNickname());
+
+        //닉네임에 해당하는 사용자가 존재하는 경우
+        if(byNickname.isPresent()) {
+            User user = byNickname.get();
+            if(!user.getPassword().equals(loginRequest.getPassword())) {
+                throw new InvalidPasswordException();
+            }
+            return user;
+        }
+
+        return signUp(loginRequest.getNickname(), loginRequest.getPassword());
+    }
+
+    private User signUp(String nickname, String password) {
+        User user = User.builder()
+                .nickname(nickname)
+                .password(password)
+                .build();
+
+        return userRepository.save(user);
+    }
 }
+
