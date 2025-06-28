f31239bfa0da0de5bb78125f64b58349d6be4841
kkb <gimgyubin1225@gmail.com>
2025-06-29 00:06:34 +0900

    [FEAT] User 예외 처리 기능 추가
    존재하지 않는 경우

--- src/main/java/Hackathon/DopamineMarket/domain/user/exception/handler/UserControllerAdvice.java
+++ src/main/java/Hackathon/DopamineMarket/domain/user/exception/handler/UserControllerAdvice.java
@@ -1,5 +1,6 @@
 package Hackathon.DopamineMarket.domain.user.exception.handler;
 
+import Hackathon.DopamineMarket.domain.user.exception.UserNotFoundException;
 import Hackathon.DopamineMarket.domain.user.exception.InvalidPasswordException;
 import Hackathon.DopamineMarket.global.response.BaseErrorResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static Hackathon.DopamineMarket.global.response.status.BaseExceptionResponseStatus.INVALID_PASSWORD_ERROR;
+import static Hackathon.DopamineMarket.global.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Slf4j
@@ -20,8 +22,14 @@ public class UserControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidPasswordException.class)
     public BaseErrorResponse handleInvalidPassword(InvalidPasswordException e) {
-        log.error("[InvalidPasswordException] ", e);
+        log.error("[InvalidPasswordException]", e);
         return new BaseErrorResponse(INVALID_PASSWORD_ERROR, e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public BaseErrorResponse handleUserNotFound(UserNotFoundException e) {
+        log.error("[UserNotFoundException]", e);
+        return new BaseErrorResponse(USER_NOT_FOUND, e.getMessage());
+    }
 }
