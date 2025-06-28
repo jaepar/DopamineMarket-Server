36d9b0dd83ceb1f6e1d52228913011e788ada65b
jiyun921 <ryn0921@naver.com>
2025-06-29 00:01:12 +0900

    [feat]: AppController 구현

--- src/main/java/Hackathon/DopamineMarket/domain/app/controller/AppController.java
+++ src/main/java/Hackathon/DopamineMarket/domain/app/controller/AppController.java
@@ -1,14 +1,12 @@
 package Hackathon.DopamineMarket.domain.app.controller;
 
 import Hackathon.DopamineMarket.domain.app.dto.request.PostAppCreateRequest;
-import Hackathon.DopamineMarket.domain.app.dto.response.AppListResponse;
+import Hackathon.DopamineMarket.domain.app.dto.response.GetAppListResponse;
+import Hackathon.DopamineMarket.domain.app.dto.response.PostAppCreateResponse;
 import Hackathon.DopamineMarket.domain.app.service.AppService;
 import Hackathon.DopamineMarket.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/apps")
@@ -17,7 +15,12 @@ public class AppController {
     private final AppService appService;
 
     @PostMapping
-    public BaseResponse<AppListResponse>  createApp(@RequestBody PostAppCreateRequest request) {
+    public BaseResponse<PostAppCreateResponse>  createApp(@RequestBody PostAppCreateRequest request) {
         return BaseResponse.ok(appService.createApp(request));
+    }
+
+    @GetMapping
+    public BaseResponse<GetAppListResponse> getAppList(@RequestParam Long userId) {
+        return BaseResponse.ok(appService.getApps(userId));
     }
 }
