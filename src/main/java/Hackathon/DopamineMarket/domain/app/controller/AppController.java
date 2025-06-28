538d877536fc27521f334c928202b758d496f7c4
jiyun921 <ryn0921@naver.com>
2025-06-28 21:26:40 +0900

    [feat]: AppController 구현

---
+++ src/main/java/Hackathon/DopamineMarket/domain/app/controller/AppController.java
@@ -0,0 +1,28 @@
+package Hackathon.DopamineMarket.domain.app.controller;
+
+import Hackathon.DopamineMarket.domain.app.dto.request.PostAppCreateRequest;
+import Hackathon.DopamineMarket.domain.app.dto.response.PostAppCreateResponse;
+import Hackathon.DopamineMarket.domain.app.service.AppService;
+import Hackathon.DopamineMarket.domain.user.domain.User;
+import Hackathon.DopamineMarket.domain.user.repository.UserRepository;
+import Hackathon.DopamineMarket.global.response.BaseResponse;
+import Hackathon.DopamineMarket.global.response.status.BaseExceptionResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apps")
+@RequiredArgsConstructor
+public class AppController {
+    private final AppService appService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<PostAppCreateResponse>> createApp(@RequestBody PostAppCreateRequest request) {
+        BaseResponse<PostAppCreateResponse> response = BaseResponse.ok(appService.createApp(request));
+        return ResponseEntity.ok(response);
+    }
+}
