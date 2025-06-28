a434e21e9128b318f316cfbf2765f94f1a9e45a0
Jay Park <pjh6183@naver.com>
2025-06-28 21:17:28 +0900

    [feat]: 루틴 등록 controller 구현

--- src/main/java/Hackathon/DopamineMarket/domain/routine/controller/RoutineController.java
+++ src/main/java/Hackathon/DopamineMarket/domain/routine/controller/RoutineController.java
@@ -1,4 +1,25 @@
 package Hackathon.DopamineMarket.domain.routine.controller;
 
+import Hackathon.DopamineMarket.domain.routine.dto.request.PostRoutineCreateRequest;
+import Hackathon.DopamineMarket.domain.routine.dto.response.PostRoutineCreateResponse;
+import Hackathon.DopamineMarket.domain.routine.service.RoutineService;
+import Hackathon.DopamineMarket.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/routines")
+@RequiredArgsConstructor
 public class RoutineController {
+
+    private final RoutineService routineService;
+
+    @PostMapping
+    public BaseResponse<PostRoutineCreateResponse> createRoutine(@RequestBody PostRoutineCreateRequest request) {
+        return BaseResponse.ok(routineService.createRoutine(request));
+    }
+
 }
