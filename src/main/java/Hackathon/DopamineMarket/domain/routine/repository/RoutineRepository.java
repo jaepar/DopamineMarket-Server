8b04a5be35f5fb6bcf5d9240545c1269bddfb39e
Jay Park <pjh6183@naver.com>
2025-06-28 22:08:57 +0900

    [feat]: routine list repository 구현

--- src/main/java/Hackathon/DopamineMarket/domain/routine/repository/RoutineRepository.java
+++ src/main/java/Hackathon/DopamineMarket/domain/routine/repository/RoutineRepository.java
@@ -1,8 +1,11 @@
 package Hackathon.DopamineMarket.domain.routine.repository;
 
 import Hackathon.DopamineMarket.domain.routine.domain.Routine;
+import Hackathon.DopamineMarket.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoutineRepository extends JpaRepository<Routine, Long> {
+import java.util.List;
 
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
+    List<Routine> findAllByUser(User user);
 }
