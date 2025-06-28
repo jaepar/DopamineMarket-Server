a1a89fbcf264c000c47ae42781b2c29c1ab63693
Jay Park <pjh6183@naver.com>
2025-06-28 22:09:23 +0900

    [feat]: routine list service 로직 구현

--- src/main/java/Hackathon/DopamineMarket/domain/routine/service/RoutineService.java
+++ src/main/java/Hackathon/DopamineMarket/domain/routine/service/RoutineService.java
@@ -3,7 +3,9 @@ package Hackathon.DopamineMarket.domain.routine.service;
 import Hackathon.DopamineMarket.domain.routine.domain.Routine;
 import Hackathon.DopamineMarket.domain.routine.domain.RoutineCategory;
 import Hackathon.DopamineMarket.domain.routine.dto.request.PostRoutineCreateRequest;
+import Hackathon.DopamineMarket.domain.routine.dto.response.GetRoutineListResponse;
 import Hackathon.DopamineMarket.domain.routine.dto.response.PostRoutineCreateResponse;
+import Hackathon.DopamineMarket.domain.routine.dto.response.RoutineItem;
 import Hackathon.DopamineMarket.domain.routine.exception.InvalidRoutineCategoryException;
 import Hackathon.DopamineMarket.domain.routine.exception.UserNotFoundException;
 import Hackathon.DopamineMarket.domain.routine.repository.RoutineRepository;
@@ -13,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static Hackathon.DopamineMarket.global.response.status.BaseExceptionResponseStatus.CATEGORY_NOT_FOUND;
 import static Hackathon.DopamineMarket.global.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
@@ -46,6 +50,35 @@ public class RoutineService {
 
         Routine saved = routineRepository.save(routine);
         return PostRoutineCreateResponse.of(saved.getRoudtineId());
+    }
+
+    public GetRoutineListResponse getRoutines(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+
+        List<Routine> routines = routineRepository.findAllByUser(user);
+
+        List<RoutineItem> fixed = new ArrayList<>();
+        List<RoutineItem> todayOnly = new ArrayList<>();
+
+        for (Routine r : routines) {
+            RoutineItem item = new RoutineItem(
+                    r.getRoudtineId(),
+                    r.getTitle(),
+                    r.getCategory().name(),
+                    r.getTimer(),
+                    r.isDaily(),
+                    Boolean.TRUE.equals(r.getCompleted()),
+                    r.getCreatedAt().toString()
+            );
+            if (r.isDaily()) {
+                todayOnly.add(item); // 오늘만 수행되는 루틴
+            } else {
+                fixed.add(item);     // 고정 루틴 (매일 반복)
+            }
+        }
+
+        return GetRoutineListResponse.of(fixed, todayOnly);
     }
 
 }
