6388c9e12a595d9a9ba500d80b7f884a96de79da
jiyun921 <ryn0921@naver.com>
2025-06-29 00:00:53 +0900

    [feat]: Apprepository에 findAll() 추가

--- src/main/java/Hackathon/DopamineMarket/domain/app/repository/AppRepository.java
+++ src/main/java/Hackathon/DopamineMarket/domain/app/repository/AppRepository.java
@@ -1,8 +1,12 @@
 package Hackathon.DopamineMarket.domain.app.repository;
 
 import Hackathon.DopamineMarket.domain.app.domain.App;
+import Hackathon.DopamineMarket.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AppRepository extends JpaRepository<App, Long> {
     boolean existsByUserUserIdAndAppName(Long userId, String appName);
+    List<App> findAllByUser(User user);
 }
