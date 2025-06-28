9d9a78044e03ffffe455af30c4351e6d8d039d72
jiyun921 <ryn0921@naver.com>
2025-06-29 03:09:17 +0900

    [feat]: 내 작업 내용 저장

--- src/main/java/Hackathon/DopamineMarket/domain/routine/controller/RoutineController.java
+++ src/main/java/Hackathon/DopamineMarket/domain/routine/controller/RoutineController.java
@@ -1,4 +1,40 @@
 package Hackathon.DopamineMarket.domain.routine.controller;
 
+import Hackathon.DopamineMarket.domain.routine.dto.request.PostRoutineCreateRequest;
+import Hackathon.DopamineMarket.domain.routine.dto.response.GetRoutineListResponse;
+import Hackathon.DopamineMarket.domain.routine.dto.response.PostRoutineCreateResponse;
+import Hackathon.DopamineMarket.domain.routine.service.RoutineService;
+import Hackathon.DopamineMarket.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/routines")
+@RequiredArgsConstructor
 public class RoutineController {
+
+    private final RoutineService routineService;
+
+    @PostMapping
+    public BaseResponse<PostRoutineCreateResponse> createRoutine(@RequestBody PostRoutineCreateRequest request) {
+        return BaseResponse.ok(routineService.createRoutine(request));
+    }
+
+    @GetMapping
+    public BaseResponse<GetRoutineListResponse> getRoutineList(@RequestParam("userId") Long userId) {
+        return BaseResponse.ok(routineService.getRoutines(userId));
+    }
+
+    @PostMapping("/{routineId}/complete")
+    public BaseResponse<Void> completeRoutine(@PathVariable(name = "routineId") Long routineId) {
+        routineService.completeRoutine(routineId);
+        return BaseResponse.ok(null);
+    }
+
+    @DeleteMapping("/{routineId}")
+    public BaseResponse<Void> deleteRoutine(@PathVariable(name = "routineId") Long routineId) {
+        routineService.deleteRoutine(routineId);
+        return BaseResponse.ok(null);
+    }
+
 }
